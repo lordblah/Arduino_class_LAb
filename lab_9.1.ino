1154d082c2eecac4d547bfd337b584b8996228ff
Jonathan Hernandez <helharatheband@gmail.com>
2018-05-02 18:24:07 -0700

    Add files via upload

---
+++ lab_9.1.ino
@@ -0,0 +1,26 @@
+void setup() {
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int myArrary[10];
+
+  fillArrary(myArrary, 10);
+
+  for(int i = 0; i < 10; i++)
+  {
+    Serial.println(myArrary[i]);
+  }
+  delay(1000);
+}
+
+void fillArrary(int theArrary[], int n)
+{
+  for(int i = 0; i < n; i++)
+  {
+    theArrary[i] = 10 * i;
+  }
+}
+
